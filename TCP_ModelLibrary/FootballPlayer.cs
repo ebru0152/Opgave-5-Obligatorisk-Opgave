d952d997fd1b89837e44e77b72f0deef65952d4b
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-10-01 19:42:10 +0200

    Add files via upload

---
+++ TCP_ModelLibrary/FootballPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TCP_ModelLibrary
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer()
+        {
+            
+        }
+        public FootballPlayer(int id, string name, double price, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+    }
+}
